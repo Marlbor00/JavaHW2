4e257f3a7fdfbe0412887923c297f38bf37b16fd
Marlboro <marlb0ro@mail.ru>
2022-07-31 09:32:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int amount = 25_000;
+        int miles = amount/20;
+
+        System.out.println("Стоимость билета: " + amount + " рублей");
+        System.out.println("Количество бонусных миль: " + miles + " бонусов");
+    }
+}
